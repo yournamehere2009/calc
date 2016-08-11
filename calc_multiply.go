e17705f75c49466bdc8c4eeeda411994acf5c862
Mike Chinn <yournamehere2009@gmail.com>
2016-08-10 17:31:05 -0700

    Initial Commit

---
+++ calc_multiply.go
@@ -0,0 +1,6 @@
+package calc
+
+// Multiply takes two numbers and adds them together
+func Multiply(a float64, b float64) float64 {
+    return a*b;
+}
---
+++ calc_subtract.go
@@ -0,0 +1,6 @@
+package calc
+
+//Subtract does
+func Subtract(a float64, b float64) float64 {
+    return a-b;
+}
